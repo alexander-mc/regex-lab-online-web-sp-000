a3850bf6948e70f91af781a40f3b03ffb9368913
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-19 23:24:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,8 +3,13 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/(^un\w+ing$/))
+  puts text.split
+#  text.scan(/^un\w+ing$/)
 end
+
+text1 = "underling unpopling unpoppy sunpoping unpopplingy"
+words_starting_with_un_and_ending_with_ing)(text1)
+
 
 def words_five_letters_long(text)
 
