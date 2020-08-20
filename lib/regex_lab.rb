721eb150df135dce3e3336d517baedcedbee6642
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 00:14:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\b[Uu]n[a-z]*ing\b/)
+  text.scan(/\b[Uu]n\w*ing\b/)
 #   text.split.select do |word|
 #     word.scan(/^un+\w+ing$/).length == 1
 #   end
