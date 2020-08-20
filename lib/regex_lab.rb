809f6635d059291a5d7bfc67544a2f6f7302604e
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 00:17:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
   text.scan(/\w{5}/)
 end
 
-text = [five six seven nine]
+text = "five six seven nine"
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
