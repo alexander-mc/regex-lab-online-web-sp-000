1d8a0026361c5e4d16166a6c294012933b54c9b9
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 00:00:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
    text.split.select do |word|
-     if word.scan(/^un+\w+ing$/) == true
+     word.scan(/^un+\w+ing$/) == true
      end
    end
 #  text.scan(/^un\w+ing$/)
